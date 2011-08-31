6c5bd283132a844666ccfed8eb7863981cbdc80d
Matthew Scott <mjs@mjs-svc.com>
2011-08-30 20:26:44 -0600

    Work

--- characters/views.py
+++ characters/views.py
@@ -1,12 +1,19 @@
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.models import User
+from django.contrib.flatpages.models import FlatPage
 from charref.characters.models import *
 
 def front(request):
     pass
 
 def show_user(request, username):
-    pass
+    user = get_object_or_404(User, username = username)
+    return render_to_response('characters/user/show.html', context_instance = RequestContext(request, {'user': user}))
 
+@login_required
 def edit_user(request):
     pass
 
@@ -16,56 +23,72 @@ def register(request):
 ##
 
 def show_character(request, character_id):
-    pass
+    character = get_object_or_404(Character, id = character_id)
+    return render_to_response('characters/character/show.html', context_instance = RequestContext(request, {'character': character}))
 
+@login_required
 def edit_character(request, character_id):
     pass
 
+@login_required
 def delete_character(request, character_id):
     pass
 
+@login_required
 def create_character(request, character_id):
     pass
 
 ##
 
 def show_morph(request, morph_id):
-    pass
+    morph = get_object_or_404(Morph, id = morph_id)
+    return render_to_response('characters/morph/show.html', context_instance = RequestContext(request,  {'morph': morph}))
 
+@login_required
 def edit_morph(request, morph_id):
     pass
 
+@login_required
 def delete_morph(request, morph_id):
     pass
 
+@login_required
 def create_morph(request, morph_id):
     pass
 
 ##
 
-def show_description(request, character_id):
+def show_description(request, description_id):
+    description = get_object_or_404(Description, id = description_id)
+    return render_to_response('characters/description/show.html', context_instance = RequestContext(request, {'description': description}))
+
+@login_required
+def edit_description(request, description_id):
     pass
 
-def edit_description(request, character_id):
+@login_required
+def delete_description(request, description_id):
     pass
 
-def delete_description(request, character_id):
-    pass
-
-def create_description(request, character_id):
+@login_required
+def create_description(request, description_id):
     pass
 
 ##
 
 def show_location(request, location_id):
-    pass
+    location = get_object_or_404(Location, id = location_id)
+    return render_to_response('characters/location/show.html', context_instance = RequestContext(request, {'location': location}))
 
+@login_required
 def edit_location(request, location_id):
     pass
 
+@login_required
 def delete_location(request, location_id):
     pass
 
+@login_required
 def create_location(request, location_id):
     pass
 
